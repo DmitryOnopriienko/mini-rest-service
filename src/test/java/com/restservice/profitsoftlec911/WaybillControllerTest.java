ecf63bd79c811866ef48aba1167a9ef8f64f80df
Dmitry Onopriienko <dmitry.onopriienko@gmail.com>
2023-01-22 13:46:10 +0200

    Add all tests for WaybillController

--- src/test/java/com/restservice/profitsoftlec911/WaybillControllerTest.java
+++ src/test/java/com/restservice/profitsoftlec911/WaybillControllerTest.java
@@ -2,7 +2,10 @@ package com.restservice.profitsoftlec911;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.restservice.profitsoftlec911.dto.RestResponse;
 import com.restservice.profitsoftlec911.entities.Customer;
+import com.restservice.profitsoftlec911.entities.Waybill;
+import com.restservice.profitsoftlec911.exceptions.NotFoundException;
 import com.restservice.profitsoftlec911.repositories.CustomerRepository;
 import com.restservice.profitsoftlec911.repositories.WaybillRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -15,10 +18,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -48,8 +54,9 @@ public class WaybillControllerTest {
   }
 
   @AfterEach
-  void clearRepository() {
+  void clearRepositories() {
     waybillRepository.deleteAll();
+    customerRepository.deleteAll();
   }
 
   @Test
@@ -127,6 +134,161 @@ public class WaybillControllerTest {
                     }
                     """.formatted(maxCustomerId + 1)))
             .andExpect(status().isNotFound());
+  }
+
+  @Test
+  void testCreateValidWaybill() throws Exception {
+    int customerId = addCustomer();
+
+    String type = "air-drop";
+    String price = "70.99";
+    String date = "2022-07-07";
+
+    MvcResult result = mvc.perform(post(WaybillControllerLinksConstants.WAYBILL_CREATE)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("""
+                    {
+                      "type": "%s",
+                      "price": %s,
+                      "date": "%s",
+                      "customerId": %d
+                    }
+                    """.formatted(type, price, date, customerId)))
+            .andExpect(status().isCreated())
+            .andReturn();
+
+    RestResponse response = parseResponse(result, RestResponse.class);
+    int id = Integer.parseInt(response.getResult());
+    assertThat(id).isGreaterThanOrEqualTo(1);
+
+    Waybill waybill = waybillRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException("Waybill with id %d not found".formatted(id)));
+
+    assertEquals(type, waybill.getType());
+    assertEquals(Double.valueOf(price), waybill.getPrice());
+    assertEquals(date, waybill.getDate().toString());
+    assertEquals(customerId, waybill.getCustomer().getId());
+
+    mvc.perform(get(WaybillControllerLinksConstants.WAYBILLS + "/%d".formatted(id))
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+  }
+
+  @Test
+  void testInsert5ValidAnd2InvalidWaybills() throws Exception {
+    int customerId = addCustomer();
+
+    for (int i = 0; i < 5; i++) {
+      mvc.perform(post(WaybillControllerLinksConstants.WAYBILL_CREATE)
+                      .contentType(MediaType.APPLICATION_JSON)
+                      .content("""
+                              {
+                                  "type": "air-drop",
+                                  "price": 70.99,
+                                  "date": "2022-07-07",
+                                  "customerId": %d
+                              }
+                              """.formatted(customerId)))
+              .andExpect(status().isCreated());
+    }
+
+    for (int i = 0; i < 2; i++) {
+      mvc.perform(post(WaybillControllerLinksConstants.WAYBILL_CREATE)
+                      .contentType(MediaType.APPLICATION_JSON)
+                      .content("""
+                              {
+                                  "type": "air-drop",
+                                  "price": 70.99,
+                                  "date": "2022-07-07",
+                                  "customerId": -1
+                              }
+                              """))
+              .andExpect(status().isBadRequest());
+    }
+
+    List<Waybill> waybills = waybillRepository.findAll();
+    assertEquals(5, waybills.size());
+  }
+
+  @Test
+  void testAddAndDelete() throws Exception {
+    int customerId = addCustomer();
+
+    MvcResult result = mvc.perform(post(WaybillControllerLinksConstants.WAYBILL_CREATE)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("""
+                              {
+                                  "type": "air-drop",
+                                  "price": 70.99,
+                                  "date": "2022-07-07",
+                                  "customerId": %d
+                              }
+                              """.formatted(customerId)))
+            .andExpect(status().isCreated())
+            .andReturn();
+
+    RestResponse response = parseResponse(result, RestResponse.class);
+    int id = Integer.parseInt(response.getResult());
+
+    mvc.perform(delete(WaybillControllerLinksConstants.WAYBILLS + "/%d".formatted(id))
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+  }
+
+  @Test
+  void testUpdate() throws Exception {
+    int customerId = addCustomer();
+
+    MvcResult result = mvc.perform(post(WaybillControllerLinksConstants.WAYBILL_CREATE)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("""
+                              {
+                                  "type": "air-drop",
+                                  "price": 70.99,
+                                  "date": "2022-07-07",
+                                  "customerId": %d
+                              }
+                              """.formatted(customerId)))
+            .andExpect(status().isCreated())
+            .andReturn();
+
+    RestResponse response = parseResponse(result, RestResponse.class);
+    int id = Integer.parseInt(response.getResult());
+
+    mvc.perform(
+            get(WaybillControllerLinksConstants.WAYBILLS + "/%d".formatted(id))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isOk());
+
+    Waybill waybill = waybillRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException("Waybill with id %d not found".formatted(id)));
+
+    String newPrice = "1.99";
+    String newType = "sea-shipping";
+
+    mvc.perform(patch(WaybillControllerLinksConstants.WAYBILLS + "/%d".formatted(id))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("""
+                    {
+                      "type": "%s",
+                      "price": %s,
+                      "date": "%s",
+                      "customerId": %d
+                    }
+                    """.formatted(
+                            newType,
+                            newPrice,
+                            waybill.getDate().toString(),
+                            waybill.getCustomer().getId()
+                    )
+            )).andExpect(status().isOk());
+
+    waybill = waybillRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException("Waybill with id %d not found".formatted(id)));
+
+    assertEquals(newType, waybill.getType());
+    assertEquals(Double.valueOf(newPrice), waybill.getPrice());
   }
 
   private <T>T parseResponse(MvcResult mvcResult, Class<T> c) {
