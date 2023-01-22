8a4482bfe237eab5eb56d2c2b6383b5b5b8d3335
Dmitry Onopriienko <dmitry.onopriienko@gmail.com>
2023-01-22 23:00:35 +0200

    Remove unused method with @Deprecated

--- src/main/java/com/restservice/profitsoftlec911/repositories/CustomerRepository.java
+++ src/main/java/com/restservice/profitsoftlec911/repositories/CustomerRepository.java
@@ -1,22 +1,9 @@
 package com.restservice.profitsoftlec911.repositories;
 
 import com.restservice.profitsoftlec911.entities.Customer;
-import com.restservice.profitsoftlec911.entities.Waybill;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
-  @Deprecated
-  @Query("""
-          SELECT waybill
-          FROM Waybill waybill
-          WHERE waybill.customer.id = :id
-          """)
-  List<Waybill> getWaybillsOfCustomer(int id);
-
 }
