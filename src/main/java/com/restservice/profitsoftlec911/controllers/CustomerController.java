41f815d8ca6a64612272207c838d2f4714b95166
Dmitry Onopriienko <dmitry.onopriienko@gmail.com>
2023-02-24 14:40:48 +0200

    Remove unused CrossOrigin

--- src/main/java/com/restservice/profitsoftlec911/controllers/CustomerController.java
+++ src/main/java/com/restservice/profitsoftlec911/controllers/CustomerController.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -62,15 +61,12 @@ public class CustomerController {
   }
 
   @PatchMapping("/{id}")
-  @CrossOrigin
   public void updateCustomer(@PathVariable("id") int id,
                              @RequestBody CustomerSaveDto customer) {
     customerService.update(customer, id);
   }
 
   @DeleteMapping("/{id}")
-  @CrossOrigin
-  @ResponseStatus(HttpStatus.OK)
   public void deleteCustomer(@PathVariable("id") int id) {
     customerService.delete(id);
   }
